58ccbd24112d300b9acd90d17bbde5e93170b786
Shado <blovelace84@gmail.com>
2025-04-12 13:43:32 -0400

    initial commit

---
+++ neon_racer/game_window.py
@@ -0,0 +1,9 @@
+import pygame
+from pygame.examples.sprite_texture import clock
+
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Neon Racer")
+clock = pygame.time.Clock()
+
